5567a5d2f5594f038f97d56155f2fd15b98fb90a
greg higgins <greg.higgins@v12technology.com>
2021-12-18 22:07:10 +0000

    testing

---
+++ src/test/java/com/fluxtion/example/servicestater/ServiceManagerModelATest.java
@@ -0,0 +1,65 @@
+package com.fluxtion.example.servicestater;
+
+import com.fluxtion.example.servicestater.helpers.ServiceTaskExecutor;
+import lombok.extern.java.Log;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@Log
+class ServiceManagerModelATest {
+
+    /**
+     * drawn with - https://asciiflow.com/#/
+     *
+     *
+     * +-------------+                      +------------+              +-----------+      |
+     * |             |                      |            |              |           |      |
+     * |  handler_c  |                      | handler_a  |              | handler_b |      |
+     * +---+---------+                      +----+-------+              +-----+-----+      |
+     *     |                                     |                            |            |
+     *     |    +---------+                      |        +-------+           |            |   DIRECTION OF
+     *     |    |         |                      |        |       |           |            |   EVENT FLOW
+     *     +----+ calc_c  |                      +--------+agg_AB +-----------+            |
+     *          +----+----+                               +---+---+                        |
+     *               |                                        |                            |
+     *               |                                        |                            |
+     *               |                +-----------+           |                            |
+     *               |                |           |           |                            |
+     *               +----------------+ publisher +-----------+                            |
+     *                                |           |                                        |
+     *                                +-----------+                                        v
+     *
+     *
+     */
+
+
+    private final List<StatusForService> statusList = new ArrayList<>();
+
+    @Test
+    void buildSystemController() {
+        ServiceManager serviceManager = ServiceModels.buildModelA(false, false);
+        ServiceTaskExecutor serviceTaskExecutor = new ServiceTaskExecutor();
+        serviceManager.registerStatusListener(this::recordServiceStatus);
+        //
+        assertEquals(6, statusList.size());
+        assertThat(statusList, Matchers.containsInAnyOrder(ServiceModels.allUnknownStatus().toArray()));
+    }
+
+
+    public void recordServiceStatus(List<StatusForService> statusUpdate) {
+        statusList.clear();
+        statusList.addAll(statusUpdate);
+//        log.info("Current status:\n" +
+//                statusUpdate.stream()
+//                        .map(Objects::toString)
+//                        .collect(Collectors.joining("\n"))
+//        );
+    }
+}
--- src/test/java/com/fluxtion/example/servicestater/ServiceModels.java
+++ src/test/java/com/fluxtion/example/servicestater/ServiceModels.java
@@ -1,11 +1,39 @@
 package com.fluxtion.example.servicestater;
 
-import com.fluxtion.example.servicestater.helpers.CliTestClient;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public interface ServiceModels {
+
+
+    /**
+     *
+     *
+     *
+     * +-------------+                      +------------+              +-----------+
+     * |             |                      |            |              |           |
+     * |  handler_c  |                      | handler_a  |              | handler_b |
+     * +---+---------+                      +----+-------+              +-----+-----+
+     *     |                                     |                            |
+     *     |    +---------+                      |                            |
+     *     |    |         |                      |        +-------+           |
+     *     +----+ calc_c  |                      +--------+agg_AB +-----------+
+     *          +----+----+                               +---+---+
+     *               |                                        |
+     *               |                                        |
+     *               |                +-----------+           |
+     *               |                |           |           |
+     *               +----------------+ publisher +-----------+
+     *                                |           |
+     *                                +-----------+
+     *
+     *
+     *
+     *
+     */
 
     String HANDLER_A = "handlerA";
     String HANDLER_B = "handlerB";
@@ -18,9 +46,9 @@ public interface ServiceModels {
         Service handlerA = new Service(HANDLER_A);
         Service handlerB = new Service(HANDLER_B);
         Service handlerC = new Service(HANDLER_C);
-        Service aggAB = new Service(AGG_AB, CliTestClient::notifyStartedAggAB, null, handlerA, handlerB);
+        Service aggAB = new Service(AGG_AB, handlerA, handlerB);
         Service calcC = new Service(CALC_C, handlerC);
-        Service persister = new Service(PERSISTER, CliTestClient::notifyStartedPersister, null, aggAB, calcC);
+        Service persister = new Service(PERSISTER, aggAB, calcC);
         //build and register outputs
         return new ServiceManager()
                 .addAuditLog(addAuditLog)
@@ -28,7 +56,12 @@ public interface ServiceModels {
                 .buildServiceController(persister, aggAB, calcC, handlerA, handlerB, handlerC);
     }
 
-    static List<StatusForService> allWithStatus(Service.Status status){
+    static Map<String, StatusForService> mapWithStatus(Service.Status status) {
+        return allWithStatus(status).stream()
+                .collect(Collectors.toMap(StatusForService::getServiceName, Function.identity()));
+    }
+
+    static List<StatusForService> allWithStatus(Service.Status status) {
         List<StatusForService> statusList = new ArrayList<>();
         statusList.add(new StatusForService(AGG_AB, status));
         statusList.add(new StatusForService(CALC_C, status));
@@ -39,11 +72,11 @@ public interface ServiceModels {
         return statusList;
     }
 
-    static List<StatusForService> allUnknownStatus(){
+    static List<StatusForService> allUnknownStatus() {
         return allWithStatus(Service.Status.STATUS_UNKNOWN);
     }
 
-    static List<StatusForService> allStartedStatus(){
+    static List<StatusForService> allStartedStatus() {
         return allWithStatus(Service.Status.STARTED);
     }
 
