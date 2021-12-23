518deae60e7f193916f7280ddf0147ba989746d3
greg higgins <greg.higgins@v12technology.com>
2021-12-23 12:37:53 +0000

    update license file

--- src/main/java/com/fluxtion/example/servicestater/graph/FluxtionServiceManager.java
+++ src/main/java/com/fluxtion/example/servicestater/graph/FluxtionServiceManager.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) Greg Higgins 2021.
+ *
+ * Licensed under the GNU AFFERO GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+ *
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * https://www.gnu.org/licenses/agpl-3.0.en.html
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.fluxtion.example.servicestater.graph;
 
 import com.fluxtion.compiler.Fluxtion;
