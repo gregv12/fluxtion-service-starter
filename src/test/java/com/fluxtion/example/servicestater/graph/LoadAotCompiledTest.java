a8134aca0e7e94b92fd04c3d6cdc7c5ebc9c6a0b
greg higgins <greg.higgins@v12technology.com>
2022-01-04 08:24:30 +0000

    remove logback.xml from distribution upgrade fluxtion3.1.5 Add support for using aot compiled version change lombok to provided scope export slf4j api change scope of logback to test

---
+++ src/test/java/com/fluxtion/example/servicestater/graph/LoadAotCompiledTest.java
@@ -0,0 +1,59 @@
+package com.fluxtion.example.servicestater.graph;
+
+import com.fluxtion.example.servicestater.Service;
+import com.fluxtion.example.servicestater.ServiceManager;
+import com.fluxtion.example.servicestater.ServiceStatusRecord;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+public class LoadAotCompiledTest extends FluxtionServiceManagerModelATest {
+    private static boolean Astarted = false;
+    private static boolean Bstarted = false;
+
+    @BeforeEach
+    public void init(){
+        Astarted = false;
+        Bstarted = false;
+    }
+
+    private static void buildModel(){
+        Service a = Service.builder("A").startTask(LoadAotCompiledTest::startA).build();
+        Service b = Service.builder("B").requiredServices(a).startTask(LoadAotCompiledTest::startB).build();
+        ServiceManager.compiledServiceManager(a, b);
+    }
+
+    @Test
+    public void testLoad(){
+//        buildModel();
+        ServiceManager serviceManager = ServiceManager.fromProcessor(new Processor());
+        Assertions.assertFalse(Astarted);
+        Assertions.assertFalse(Bstarted);
+
+        serviceManager.registerStatusListener(this::recordServiceStatus);
+        HashMap<String, ServiceStatusRecord> statusMap  = new HashMap<>();
+        updateStatus(statusMap, "A", Service.Status.STATUS_UNKNOWN);
+        updateStatus(statusMap, "B", Service.Status.STATUS_UNKNOWN);
+        checkStatusMatch(statusMap);
+
+        serviceManager.startService("B");
+        updateStatus(statusMap, "A", Service.Status.STARTING);
+        updateStatus(statusMap, "B", Service.Status.WAITING_FOR_PARENTS_TO_START);
+
+        serviceManager.serviceStarted("A");
+        updateStatus(statusMap, "A", Service.Status.STARTED);
+        updateStatus(statusMap, "B", Service.Status.STARTING);
+
+    }
+
+
+    public static void startA(){
+        Astarted = true;
+    }
+
+    public static void startB(){
+        Bstarted = true;
+    }
+}
